237fb66ceabcf7e35cbb62937757482bdb7fa76d
Tairan 'Ryan' Ji <tairanjiryan@gmail.com>
2024-12-14 20:58:12 -0500

    Update project name and paths in documentation and workflow configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,9 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "Mammoth Course Compass"
-copyright = "2024, Dhyey Mavani, Liam Davis, Ryan Ji, Megan Li, Brendan Shen, Michael Ubarri-Sosa"
-author = "Dhyey Mavani, Liam Davis, Ryan Ji, Megan Li, Brendan Shen, Michael Ubarri-Sosa"
+project = "Amherst Coursework"
+copyright = "2024, Dhyey Mavani, Liam Davis, Ryan Ji, Megan Li"
+author = (
+    "Dhyey Mavani, Liam Davis, Ryan Ji, Megan Li"
+)
 release = "0.0.0"
 
 # -- General configuration ---------------------------------------------------
@@ -16,7 +18,7 @@ release = "0.0.0"
 
 extensions = [
     "sphinx.ext.autodoc",
-    'sphinx.ext.napoleon',
+    "sphinx.ext.napoleon",
 ]
 
 templates_path = ["_templates"]
@@ -30,18 +32,18 @@ language = "python"
 extensions.append("sphinx_wagtail_theme")
 html_theme = "sphinx_wagtail_theme"
 html_theme_options = dict(
-    project_name = "Mammoth Course Compass",
-    logo = "",
-    logo_alt = "",
-    logo_height = 50,
-    logo_url = "/",
-    logo_width = 50,
-    github_url = "https://github.com/ac-i2i-engineering/mammoth-course-compass/",
+    project_name="Amherst Coursework",
+    logo="",
+    logo_alt="",
+    logo_height=50,
+    logo_url="/",
+    logo_width=50,
+    github_url="https://github.com/ac-i2i-engineering/amherst-coursework/",
 )
 
 html_static_path = ["_static"]
 html_css_files = [
-    'custom.css',
+    "custom.css",
 ]
 
 templates_path = ["_templates"]
@@ -51,6 +53,6 @@ import os
 import sys
 import django
 
-sys.path.insert(0, os.path.abspath("../../mammoth_course_compass_backend"))
-os.environ["DJANGO_SETTINGS_MODULE"] = "mammoth_course_compass_backend.settings"
+sys.path.insert(0, os.path.abspath("../../amherst_coursework_backend"))
+os.environ["DJANGO_SETTINGS_MODULE"] = "amherst_coursework_backend.settings"
 django.setup()

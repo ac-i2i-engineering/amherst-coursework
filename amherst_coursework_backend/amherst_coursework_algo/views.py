96118b0709a1344a80ef2d8debf56a8e8035a43f
Megan <my.email.for.random.websites@gmail.com>
2025-02-15 20:57:12 -0500

    Added dropdown filter menus for division and level of course

--- amherst_coursework_backend/amherst_coursework_algo/views.py
+++ amherst_coursework_backend/amherst_coursework_algo/views.py
@@ -1,22 +1,39 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Course, Department
+from .models import Course, Department, Division, CourseCode
 
 
 def home(request):
     departments = Department.objects.all().order_by("name")
-    selected_dept = request.GET.get("department", "")
+    divisions = Division.objects.all().order_by("name")
+    levels = ["100", "200", "300", "400"]
+    selected_depts = request.GET.getlist("department", "")
+    selected_div = request.GET.get("division", "")
+    selected_level = request.GET.get("level", "")
 
     courses = Course.objects.all().order_by("courseName")
-    if selected_dept:
-        courses = courses.filter(departments__code=selected_dept)
+    if selected_depts:
+        courses = courses.filter(departments__code__in=selected_depts)
+    if selected_div:
+        courses = courses.filter(divisions__name=selected_div)
+    if selected_level:
+        level_prefix = selected_level[0]
+
+
+        courses = courses.filter(courseCodes__value__contains=f"-{level_prefix}").distinct()
+        print(f"Filtered courses: {[c.courseName for c in courses]}")
+
 
     return render(
         request,
         "amherst_coursework_algo/home.html",
         {
             "departments": departments,
+            "divisions": divisions,
+            "levels": levels,
             "courses": courses,
-            "selected_dept": selected_dept,
+            "selected_depts": selected_depts,
+            "selected_div": selected_div,
+            "selected_level": selected_level,
         },
     )
 

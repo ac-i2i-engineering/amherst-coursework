72315c9d213465800783bc4a7cd951aff7dee4d1
Megan <my.email.for.random.websites@gmail.com>
2025-03-02 03:19:59 -0500

    Modularized all search filters (name, depts, division,s keywords, half, cosine similarity)

--- amherst_coursework_backend/amherst_coursework_algo/urls.py
+++ amherst_coursework_backend/amherst_coursework_algo/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("details/<int:course_id>/", views.course_details, name="course_details"),
     path("cart-courses/", views.get_cart_courses, name="cart_courses"),
+    path('api/course/<int:course_id>/', views.get_course_by_id, name='get_course_by_id'),
+
 ]

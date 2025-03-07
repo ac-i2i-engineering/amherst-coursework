9ceda249bc72d98c6517425d8375def84aeea16a
Megan <my.email.for.random.websites@gmail.com>
2025-03-07 13:22:00 -0500

    Added unit tests for masked filter functionality

--- amherst_coursework_backend/amherst_coursework_algo/urls.py
+++ amherst_coursework_backend/amherst_coursework_algo/urls.py
@@ -7,8 +7,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("details/<int:course_id>/", views.course_details, name="course_details"),
     path("cart-courses/", views.get_cart_courses, name="cart_courses"),
-    path(
-        "api/course/<int:course_id>/", views.get_course_by_id, name="get_course_by_id"
-    ),
     path("api/masked_filter/", masked_filters.filter, name="filter_courses"),
 ]
